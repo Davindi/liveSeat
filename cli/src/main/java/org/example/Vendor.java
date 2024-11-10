01f1342691363ffc36e105752bae92f3070a5f42
davindi <siharahansi8@gmail.com>
2024-11-10 14:07:08 +0530

    create customer and vendor class

---
+++ cli/src/main/java/org/example/Vendor.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Vendor extends Thread{
+    private final LiveSeatTicketsPool ticketManager;
+    private final int rate;
+
+    public Vendor(LiveSeatTicketsPool ticketManager, int rate) {
+        this.ticketManager = ticketManager;
+        this.rate = rate;
+    }
+
+    @Override
+    public void run() {
+        while (!isInterrupted()) {
+            ticketManager.produce(rate);
+            try {
+                Thread.sleep(5000); // Produce tickets every 2 seconds
+            } catch (InterruptedException e) {
+                System.out.println("Producer interrupted.");
+                break;
+            }
+        }
+    }
+}
